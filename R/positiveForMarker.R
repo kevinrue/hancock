8e8e2771e10eeed19221af8a1bd9759b257bed71
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-11-28 22:55:51 +0000

    removed superfluous matrix signature method

--- R/positiveForMarker.R
+++ R/positiveForMarker.R
@@ -6,30 +6,7 @@ setMethod(
     function(x, row, threshold=0, assay="counts"){
         # Extract assay matrix
         x <- assay(x, assay)
-        # Call internal function
-        positiveForMarker(x, row, threshold)
+        isCellPositive <- (x[row, ] > threshold)
+        isCellPositive
     }
 )
-
-#' @rdname positiveForMarker
-setMethod(
-    "positiveForMarker", c("matrix"),
-    function(x, row, threshold=0){
-        # Call internal function
-        .positiveForMarker(x, row, threshold)
-    }
-)
-
-#' Detect cells positive for a given marker
-#'
-#' @rdname INTERNAL_positiveForMarker
-#'
-#' @param matrix An expression matrix.
-#' @param row Row index of a marker.
-#' @param threshold Value \emph{above which} the marker is considered detected.
-#'
-#' @return A logical vector where \code{TRUE} indicates detection of the marker.
-.positiveForMarker <- function(matrix, row, threshold) {
-    isCellPositive <- (matrix[row, ] > threshold)
-    isCellPositive
-}
