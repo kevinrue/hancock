79cd18614e9cb93764f6358bb5b4edb77de1f17a
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2019-01-06 10:07:32 +0000

    Added C++ code to create marker signature scree plot.

---
+++ src/init.cpp
@@ -0,0 +1,22 @@
+#include "hancock.h"
+
+#include "R_ext/Rdynload.h"
+#include "R_ext/Visibility.h"
+
+#define REGISTER(x, i) {#x, (DL_FUNC) &x, i}
+
+extern "C" {
+
+static const R_CallMethodDef all_call_entries[] = {
+    REGISTER(num_detected_markers, 3),
+    {NULL, NULL, 0}
+};
+
+void attribute_visible R_init_Hancock(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, all_call_entries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    R_forceSymbols(dll, TRUE);
+}
+
+}
+
